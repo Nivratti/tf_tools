e72c045216cfe9ce0921a190c37a750122e3d6bb
Nivratti <boyanenivratti@gmail.com>
2024-03-20 11:43:46 +0530

    added setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tf-tools',
+    version='0.1.0',
+    author='Nivratti Boyane',
+    author_email='your.email@example.com',
+    description='A set of tools for enhancing TensorFlow 2 projects with custom metrics, loss functions, visualizations, and attention layers.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Nivratti/tf-tools',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.0.0',
+        'matplotlib',
+        'numpy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
