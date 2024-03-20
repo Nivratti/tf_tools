2fceac82af4d913a5ebb040548e79d005e6ef724
Nivratti <boyanenivratti@gmail.com>
2024-03-20 12:00:30 +0530

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tf-tools',
+    name='tf_tools',
     version='0.1.0',
     author='Nivratti Boyane',
     author_email='your.email@example.com',
