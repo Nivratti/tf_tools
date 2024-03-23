41b5e536d3d6583dddfec3690b4fbe9b2053661e
Nivratti <boyanenivratti@gmail.com>
2024-03-23 23:19:10 +0530

    added dynamic CAM

--- tf_tools/attention_layers/cam.py
+++ tf_tools/attention_layers/cam.py
@@ -85,3 +85,82 @@ class CAM(Layer):
             A shape tuple representing the output shape of the layer.
         """
         return input_shape
+    
+
+class CAM_DynamicShape(Layer):
+    """
+    DynamicCAM is an enhanced Channel Attention Module designed to dynamically adapt to varying input shapes.
+    If we pass None to model shape and try to build model with this layer it will work fine. 
+    """
+    def __init__(self,
+                 gamma_initializer=tf.zeros_initializer(),
+                 gamma_regularizer=None,
+                 gamma_constraint=None,
+                 **kwargs):
+        super(CAM, self).__init__(**kwargs)
+        self.gamma_initializer = gamma_initializer
+        self.gamma_regularizer = gamma_regularizer
+        self.gamma_constraint = gamma_constraint
+
+    def build(self, input_shape):
+        """
+        Build the internal components of the layer based on the input shape.
+        This method initializes the weights of the layer based on the input shape.
+        
+        Args:
+            input_shape: Shape tuple of the input feature maps.
+        """
+        # Initialize gamma, a trainable weight for the output
+        self.gamma = self.add_weight(shape=(1,),
+                                     initializer=self.gamma_initializer,
+                                     name='gamma',
+                                     regularizer=self.gamma_regularizer,
+                                     constraint=self.gamma_constraint)
+        super(CAM, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        """
+        The logic of the layer's forward pass, which computes the output based on the 
+        input tensors and the layer's parameters.
+        
+        Args:
+            inputs: Input tensor, the feature map from a convolutional layer.
+            **kwargs: Additional keyword arguments.
+            
+        Returns:
+            Tensor of the same shape as `inputs`, representing the attention-weighted
+            feature map.
+        """
+        input_shape = tf.shape(inputs)
+        batch_size, height, width, channels = input_shape[0], input_shape[1], input_shape[2], input_shape[3]
+
+        # Reshape inputs dynamically
+        vec_a = tf.reshape(inputs, [batch_size, height * width, channels])
+
+        # Compute the channel-wise attention map
+        vec_aT = tf.transpose(vec_a, perm=[0, 2, 1])
+        aTa = tf.matmul(vec_aT, vec_a)
+        softmax_aTa = Activation('softmax')(aTa)
+
+        # Apply the attention map to the input features
+        aaTa = tf.matmul(softmax_aTa, vec_aT)
+        aaTa = tf.transpose(aaTa, perm=[0, 2, 1])
+        
+        # Reshape back to the original input shape dynamically
+        aaTa = tf.reshape(aaTa, [batch_size, height, width, channels])
+
+        # Output is a weighted sum of the input and the attention-weighted feature map
+        out = self.gamma * aaTa + inputs
+        return out
+
+    def compute_output_shape(self, input_shape):
+        """
+        Computes the output shape of the layer given the input shape.
+        
+        Args:
+            input_shape: Shape tuple of the input feature maps.
+            
+        Returns:
+            A shape tuple representing the output shape of the layer.
+        """
+        return input_shape
