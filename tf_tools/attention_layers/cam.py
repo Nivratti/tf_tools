6f9173751b063623b6eac32c9f0d88573ebadef5
Nivratti Boyane <boyanenivratti@gmail.com>
2024-03-24 23:58:16 +0530

    Update cam.py

--- tf_tools/attention_layers/cam.py
+++ tf_tools/attention_layers/cam.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Activation
+from keras.layers import Layer, Activation
 
 
 class CAM(Layer):
@@ -44,6 +44,7 @@ class CAM(Layer):
                                      constraint=self.gamma_constraint)
         super(CAM, self).build(input_shape)
 
+    @tf.function()
     def call(self, inputs, **kwargs):
         """
         The logic of the layer's forward pass, which computes the output based on the 
@@ -118,6 +119,7 @@ class CAM_DynamicShape(Layer):
                                      constraint=self.gamma_constraint)
         super(CAM_DynamicShape, self).build(input_shape)
 
+    @tf.function()
     def call(self, inputs, **kwargs):
         """
         The logic of the layer's forward pass, which computes the output based on the 
