a8ebb1fb1d95cf2e5acdbf7f35ac294b1cacc88a
Nivratti Boyane <boyanenivratti@gmail.com>
2024-03-24 21:33:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/Nivratti/tf-tools',
     packages=find_packages(),
     install_requires=[
-        'tensorflow>=2.0.0',
+        'tensorflow',
         'matplotlib',
         'numpy',
     ],
